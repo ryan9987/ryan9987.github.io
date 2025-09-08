249ddd73918bea779a763f6677ffbc43d7bf4eee
Ryan O'Connor <oconnorr709@gmail.com>
2025-09-08 13:26:14 -0400

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/ryan9987.github.io',
   build: {
     outDir: 'dist' // Output to the local dist folder
   }
