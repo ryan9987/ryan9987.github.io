ea14efb6bb006470b472247ce0612035fa14e064
Ryan O'Connor <oconnorr709@gmail.com>
2025-09-08 14:35:26 -0400

    updates

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ryan9987.github.io',
+  base: './',
   build: {
-    outDir: 'dist' // Output to the local dist folder
+    outDir: 'dist',
+    assetsDir: 'assets'
   }
 })
