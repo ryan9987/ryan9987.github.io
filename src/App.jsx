35218280e237fbdce5313bd2c3ef03ef92f0914a
Ryan O'Connor <oconnorr709@gmail.com>
2025-09-09 20:32:02 -0400

    removed project

--- src/App.jsx
+++ src/App.jsx
@@ -46,12 +46,7 @@ function App() {
       tech: ['Express', 'MongoDB', 'Node.js', "CSS", "JavaScript", "HTML"],
       link: 'https://fractional-currency-fe.onrender.com/'
     },
-    {
-      title: 'Recoil Script for Rust',
-      description: 'Created a mouse automation script that programmatically offsets cursor movement to simulate recoil compensation, showcasing skills in scripting, event handling, and real-time system interaction.',
-      tech: ['C++', 'WIN32 API', 'DirectX9', "ImGui", "Multithreading"],
-      link: "https://github.com/ryan9987/mousescript"
-    },
+
     {
       title: 'Penn State Marketplace (In Progress)',
       description: 'A full-stack marketplace for Penn State students that features secure PSU email verification, categorized listings, and local exchange tools.',
